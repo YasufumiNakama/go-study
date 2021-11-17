e03c304955bc74a19d46133f8ec5c3f6b6ca12ee
YasufumiNakama <nakamayasufumi@NAKAMAnoMacBook-Pro.local>
2021-11-18 03:39:21 +0900

    add ex02

---
+++ ch06/ex02/intset.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/bits"
+)
+
+type IntSet struct {
+	words []uint64
+}
+
+func main() {
+	var x, y IntSet
+	x.Add(1)
+	x.Add(144)
+	x.Add(9)
+	fmt.Println(x.String())
+	y.Add(9)
+	y.Add(42)
+	fmt.Println(y.String())
+	x.UnionWith(&y)
+	fmt.Println(x.String())
+	fmt.Println(x.Has(9), x.Has(123))
+	fmt.Println(x.String(), x.Len())
+	x.Remove(9)
+	fmt.Println(x.String())
+	x.Clear()
+	fmt.Println(x.String())
+	c1 := x.Copy()
+	c2 := x.Copy()
+	fmt.Println(&c1)
+	fmt.Println(&c2)
+	x.AddAll(2, 3, 4)
+	fmt.Println(x.String())
+}
+
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/64, uint(x%64)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+func (s *IntSet) Add(x int) {
+	word, bit := x/64, uint(x%64)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < 64; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", 64*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+func (s *IntSet) Len() int {
+	len := 0
+	for _, word := range s.words {
+		len += bits.OnesCount64(word)
+	}
+	return len
+}
+
+func (s *IntSet) Remove(x int) {
+	word, bit := x/64, uint(x%64)
+	for word < len(s.words) {
+		break
+	}
+	s.words[word] &^= 1 << bit
+}
+
+func (s *IntSet) Clear() {
+	s.words = []uint64{}
+}
+
+func (s *IntSet) Copy() *IntSet {
+	var c IntSet
+	c.words = make([]uint64, len(s.words))
+	copy(c.words, s.words)
+	return &c
+}
+
+func (s *IntSet) AddAll(vals ...int) {
+	for _, v := range vals {
+		s.Add(v)
+	}
+}

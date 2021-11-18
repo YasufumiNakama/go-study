5da8e34fc116810d0f53b63c4b0a3eaa9eb868fa
YasufumiNakama <nakamayasufumi@NAKAMAnoMacBook-Pro.local>
2021-11-19 01:29:53 +0900

    add ex03

---
+++ ch06/ex03/intset_test.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLen(t *testing.T) {
+	var tests = []struct {
+		vals []int
+		want int
+	}{
+		{[]int{1}, 1},
+		{[]int{1, 144, 9, 42}, 4},
+	}
+
+	for _, test := range tests {
+		var x IntSet
+		for _, v := range test.vals {
+			x.Add(v)
+		}
+		got := x.Len()
+		if got != test.want {
+			t.Errorf("got %d want %d", got, test.want)
+		}
+	}
+}
+
+func TestRemove(t *testing.T) {
+	var tests = []struct {
+		vals []int
+		r    int
+		want []int
+	}{
+		{[]int{1}, 1, []int{}},
+		{[]int{1, 144, 9, 42}, 144, []int{1, 9, 42}},
+	}
+
+	for _, test := range tests {
+		var x, want IntSet
+		for _, v := range test.vals {
+			x.Add(v)
+		}
+		for _, v := range test.want {
+			want.Add(v)
+		}
+		x.Remove(test.r)
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}
+
+func TestClear(t *testing.T) {
+	var tests = []struct {
+		vals []int
+		want []int
+	}{
+		{[]int{1}, []int{}},
+		{[]int{1, 144, 9, 42}, []int{}},
+	}
+
+	for _, test := range tests {
+		var x, want IntSet
+		for _, v := range test.vals {
+			x.Add(v)
+		}
+		for _, v := range test.want {
+			want.Add(v)
+		}
+		x.Clear()
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}
+
+func TestCopy(t *testing.T) {
+	var tests = []struct {
+		vals []int
+		want []int
+	}{
+		{[]int{1}, []int{1}},
+		{[]int{1, 144, 9, 42}, []int{1, 144, 9, 42}},
+	}
+
+	for _, test := range tests {
+		var x, want IntSet
+		for _, v := range test.vals {
+			x.Add(v)
+		}
+		for _, v := range test.want {
+			want.Add(v)
+		}
+		got := x.Copy()
+		if got.String() != want.String() {
+			t.Errorf("got %s want %s", got.String(), want.String())
+		}
+	}
+}
+
+func TestAddAll(t *testing.T) {
+	var tests = []struct {
+		vals []int
+		adds []int
+		want []int
+	}{
+		{[]int{1}, []int{2}, []int{1, 2}},
+		{[]int{1, 144, 9, 42}, []int{2, 3, 4}, []int{1, 2, 3, 4, 144, 9, 42}},
+	}
+
+	for _, test := range tests {
+		var x, want IntSet
+		for _, v := range test.vals {
+			x.Add(v)
+		}
+		for _, v := range test.want {
+			want.Add(v)
+		}
+		x.AddAll(test.adds...)
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}
+
+func TestIntersectWith(t *testing.T) {
+	var tests = []struct {
+		x    []int
+		y    []int
+		want []int
+	}{
+		{[]int{1, 2, 3}, []int{}, []int{}},
+		{[]int{1, 2, 3}, []int{2, 3, 4}, []int{2, 3}},
+	}
+
+	for _, test := range tests {
+		var x, y, want IntSet
+		x.AddAll(test.x...)
+		y.AddAll(test.y...)
+		want.AddAll(test.want...)
+		x.IntersectWith(&y)
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}
+
+func TestDifferenceWith(t *testing.T) {
+	var tests = []struct {
+		x    []int
+		y    []int
+		want []int
+	}{
+		{[]int{1, 2, 3}, []int{}, []int{1, 2, 3}},
+		{[]int{1, 2, 3}, []int{2, 3, 4}, []int{1}},
+	}
+
+	for _, test := range tests {
+		var x, y, want IntSet
+		x.AddAll(test.x...)
+		y.AddAll(test.y...)
+		want.AddAll(test.want...)
+		x.DifferenceWith(&y)
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}
+
+func TestSymmetricDifference(t *testing.T) {
+	var tests = []struct {
+		x    []int
+		y    []int
+		want []int
+	}{
+		{[]int{1, 2, 3}, []int{}, []int{1, 2, 3}},
+		{[]int{1, 2, 3}, []int{2, 3, 4}, []int{1, 4}},
+	}
+
+	for _, test := range tests {
+		var x, y, want IntSet
+		x.AddAll(test.x...)
+		y.AddAll(test.y...)
+		want.AddAll(test.want...)
+		x.SymmetricDifference(&y)
+		if x.String() != want.String() {
+			t.Errorf("got %s want %s", x.String(), want.String())
+		}
+	}
+}

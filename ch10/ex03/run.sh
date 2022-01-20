5a61f4f49132cea2c28a5c7ee9b7b13bb3f486e4
YasufumiNakama <nakamayasufumi@NAKAMAnoMacBook-Pro.local>
2022-01-21 07:59:57 +0900

    add ch10/ex03

---
+++ ch10/ex03/run.sh
@@ -0,0 +1,14 @@
+go build gopl.io/ch1/fetch
+./fetch http://gopl.io/ch1/helloworld?go-get=1
+
+<<COMMENT_OUT
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta name="go-import" content="gopl.io git https://github.com/adonovan/gopl.io">
+</head>
+<body>
+</body>
+</html>
+COMMENT_OUT
